0ef3fef1ad80a77b46893553bc493bd16f921953
Eric Stensland <estensland88@gmail.com>
2014-08-17 16:38:14 -0500

    new js for new routes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
+  this.route('about', {path: 'about'});
   this.route('experiments', {path: 'experiments'});
   this.route('force', {path: 'experiments/force'});
 });
@@ -26,6 +27,15 @@ App.ExperimentsRoute = Ember.Route.extend({
   }
 });
 
+App.AboutRoute = Ember.Route.extend({
+  model: function() {
+    return {
+      title: 'About',
+      info: 'This site was created for use by students to help understand Physics'
+    };
+  }
+});
+
 App.ForceController = Ember.ObjectController.extend({
   mass:         {value: 2, label: 'Mass:',        units: 'kg'},
   acceleration: {value: 1, label: 'Acceleration:', units:'m/s<sup>2</sup>'},
@@ -40,6 +50,5 @@ App.ForceController = Ember.ObjectController.extend({
     4000: 'Break a Human Femur',
     4417: 'A Measured Boxing Punch',
     34000000: 'In the Saturn V Rocket'
-    
   }
 });
