27b368e19f5cc8a0219adc9228a5114ffba77d5a
Eric Stensland <estensland88@gmail.com>
2014-08-27 21:35:15 -0500

    middle of getting it to work with ember...but jQuery is functional

--- js/app.js
+++ js/app.js
@@ -36,19 +36,39 @@ App.AboutRoute = Ember.Route.extend({
   }
 });
 
-App.ForceController = Ember.ObjectController.extend({
-  mass:         {value: 2, label: 'Mass:',        units: 'kg'},
-  acceleration: {value: 1, label: 'Acceleration:', units:'m/s<sup>2</sup>'},
-  force:                  {label: 'Force:',        units:'N'},
-  result: function() {
-      return this.get('mass.value') * this.get('acceleration.value');
-    }.property('mass.value', 'acceleration.value'),
-  forceLevels: {
-    25 : 'Break and Egg',
-    2722: 'A Measured Kung-Fu Punch',
-    3630: 'A Measured Karate Punch',
-    4000: 'Break a Human Femur',
-    4417: 'A Measured Boxing Punch',
-    34000000: 'In the Saturn V Rocket'
+App.ForceRoute = Ember.Route.extend({
+   activate: function() {
+    var currentX = 5;
+    var currentY = 5;
+    $(document).on('ready', function(){
+      bellCurve.display(5,1);
+      bellCurve.drawXZero();
+      $('#x').on('change', function(){
+        currentX = parseInt(this.value, 10);
+        bellCurve.display(currentX, currentY * 0.2);
+      });
+
+      $('#y').on('change', function(){
+        currentY = parseInt(this.value, 10);
+        bellCurve.display(currentX, currentY * 0.2);
+      });
+    });
   }
 });
+
+// App.ForceController = Ember.ObjectController.extend({
+//   mass:         {value: 2, label: 'Mass:',        units: 'kg'},
+//   acceleration: {value: 1, label: 'Acceleration:', units:'m/s<sup>2</sup>'},
+//   force:                  {label: 'Force:',        units:'N'},
+//   result: function() {
+//       return this.get('mass.value') * this.get('acceleration.value');
+//     }.property('mass.value', 'acceleration.value'),
+//   forceLevels: {
+//     25 : 'Break and Egg',
+//     2722: 'A Measured Kung-Fu Punch',
+//     3630: 'A Measured Karate Punch',
+//     4000: 'Break a Human Femur',
+//     4417: 'A Measured Boxing Punch',
+//     34000000: 'In the Saturn V Rocket'
+//   }
+// });
